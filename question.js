6f2410cc92ab48f01caa2adb455d5ec6fc51c2bd
nicholas kunz <mrnicholaskunz@gmail.com>
2017-11-15 13:42:25 +0000

    automatically backed up by learn

--- question.js
+++ question.js
@@ -19,14 +19,3 @@ class Question {
 Question._All = [];
 
 
-
-//
-// function save(){
-//     this.constructor._All.push(this)
-//   }
-//
-// function All(){
-//     this.constructor._All.push(this);
-// }
-//
-// Question._All = [];
